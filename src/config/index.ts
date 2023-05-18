759916ec05e5a1dd143d7dd5ab3785dfe868b9be
Dustin Schiffer <73139931+dschiffer12@users.noreply.github.com>
2023-05-17 22:02:20 -0600

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -59,9 +59,9 @@ export const defaultConfig: Config = {
   api: "/api",
   plugins: "/plugins",
   published: window.origin + "/p/{}/",
-  auth0Audience: "geo-forge.com",
-  auth0Domain: "geo-forge.com",
-  auth0ClientId: "reearth-authsrv-client-default",
+  auth0Audience: "https://geo-forge.com/api",
+  auth0Domain: "dev-ygb0v2204tv1h402.us.auth0.com",
+  auth0ClientId: "L6eZMuSX6NYmBbetmh4kWDg3vbC8vlFG",
   policy: {
     modalTitle: {
       en: "Re:Earth Cloud",
