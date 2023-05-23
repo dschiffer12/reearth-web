18f420d2c620060b42522a4fc2bd7a04f6d831e9
Dustin Schiffer <73139931+dschiffer12@users.noreply.github.com>
2023-05-22 22:58:31 -0600

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -61,7 +61,7 @@ export const defaultConfig: Config = {
   published: window.origin + "/p/{}/",
   auth0Audience: "https://dev-ygb0v2204tv1h402.us.auth0.com/api/v2",
   auth0Domain: "dev-ygb0v2204tv1h402.us.auth0.com",
-  auth0ClientId: "oy8Mqzq3GBLfJt7pfjk4ItEdIEYmSrkP",
+  auth0ClientId: "fO32CMJgPE78wRYFrUnOMUrKpxrGlmeO",
   policy: {
     modalTitle: {
       en: "Re:Earth Cloud",
