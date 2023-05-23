805ea94021b198e0e4182d16ee6b3dea6ec45c0a
Dustin Schiffer <73139931+dschiffer12@users.noreply.github.com>
2023-05-23 16:46:10 -0600

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -59,9 +59,9 @@ export const defaultConfig: Config = {
   api: "/api",
   plugins: "/plugins",
   published: window.origin + "/p/{}/",
-  auth0Audience: "https://dev-ygb0v2204tv1h402.us.auth0.com/api/v2",
+  auth0Audience: "geoforge.eastus.cloudapp.azure.com/api/",
   auth0Domain: "dev-ygb0v2204tv1h402.us.auth0.com",
-  auth0ClientId: "fO32CMJgPE78wRYFrUnOMUrKpxrGlmeO",
+  auth0ClientId: "y8s5xvgKUbaQit1hAQPlx34104yKawqV",
   policy: {
     modalTitle: {
       en: "Re:Earth Cloud",
